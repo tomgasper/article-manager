a58c6da8c6623e8935c9d8a9d9b98d8570db5ce2
tomek <tomgasper01@gmail.com>
2024-05-22 11:26:26 +0200

    Added Article Controller

--- ArticleManager/Controllers/ArticleController.cs
+++ ArticleManager/Controllers/ArticleController.cs
@@ -20,55 +20,7 @@ namespace ArticleManager.Controllers
         }
 
         // GET: Article
-        public async Task<IActionResult> Index()
-        {
-              return _context.Article != null ? 
-                          View(await _context.Article.ToListAsync()) :
-                          Problem("Entity set 'ArticleManagerContext.Article'  is null.");
-        }
-
-        // GET: Article/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.Article == null)
-            {
-                return NotFound();
-            }
-
-            var article = await _context.Article
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (article == null)
-            {
-                return NotFound();
-            }
-
-            return View(article);
-        }
-
-        // GET: Article/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Article/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,Author,CreatedAt,Upvotes")] Article article)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(article);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(article);
-        }
-
-        // GET: Article/Edit/5
-        public async Task<IActionResult> Edit(int? id)
+        public async Task<IActionResult> Index(int? id)
         {
             if (id == null || _context.Article == null)
             {
@@ -80,79 +32,8 @@ namespace ArticleManager.Controllers
             {
                 return NotFound();
             }
-            return View(article);
-        }
-
-        // POST: Article/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Author,CreatedAt,Upvotes")] Article article)
-        {
-            if (id != article.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(article);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ArticleExists(article.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(article);
-        }
-
-        // GET: Article/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Article == null)
-            {
-                return NotFound();
-            }
-
-            var article = await _context.Article
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (article == null)
-            {
-                return NotFound();
-            }
 
             return View(article);
-        }
-
-        // POST: Article/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            if (_context.Article == null)
-            {
-                return Problem("Entity set 'ArticleManagerContext.Article'  is null.");
-            }
-            var article = await _context.Article.FindAsync(id);
-            if (article != null)
-            {
-                _context.Article.Remove(article);
-            }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
         }
 
         private bool ArticleExists(int id)
